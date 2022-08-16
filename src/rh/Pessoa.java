affb36156f2321f8aefb8f35c725ffca536d8780
Roberto Bomfim <bomfim062@gmail.com>
2022-08-16 12:12:27 -0300

    Atualização treino

--- src/rh/Pessoa.java
+++ src/rh/Pessoa.java
@@ -1,14 +1,13 @@
-package calculadora.rh;
+package rh;
 
 import java.util.Objects;
 
 public class Pessoa {
-
     protected String nome;
     private int idade;
 
     public Pessoa(String nome, int idade) {
-        if( Objects.isNull(nome))
+        if( Objects.isNull(nome) || idade < 18)
             throw new RuntimeException("Error");
         this.nome = nome;
         this.idade = idade;
@@ -18,17 +17,9 @@ public class Pessoa {
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome) { this.nome = nome;}
 
+    public int getIdade() { return idade; }
 
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+    public void setIdade(int idade) { this.idade = idade; }
 }
--- src/rh/PessoaTeste.java
+++ src/rh/PessoaTeste.java
@@ -1,11 +1,13 @@
-package calculadora.rh;
+package rh;
 
 public class PessoaTeste {
     public static void main(String[] args) {
 
-        Pessoa pessoa1 = new Pessoa(null, 29);
+        Pessoa pessoa1 = new Pessoa("Roberto", 29);
         String nome = pessoa1.getNome();
+        int idade = pessoa1.getIdade();
 
         System.out.println(nome);
+        System.out.println(idade);
     }
 }
